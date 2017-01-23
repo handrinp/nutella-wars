35cfad8c53c08cd9175c456525ff853b5da7511e
handrinp <handrinp@uwec.edu>
2017-01-22 18:57:35 -0600

    indentation

--- src/main/java/com/nutella/Build.java
+++ src/main/java/com/nutella/Build.java
@@ -3,5 +3,5 @@ package com.nutella;
 public class Build {
     public static final int OFFENSIVE = 0;
     public static final int DEFENSIVE = 1;
-    public static final int MAGICAL = 2;
+    public static final int   MAGICAL = 2;
 }
